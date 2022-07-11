f160494d8f7d7f157051b35a3ae71ad789f1e935
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-11 14:09:15 -0500

    Add formatSpots function, so that the text showing the number of spots remaining is dynamically generated from props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,14 +2,24 @@ import React from "react";
 import "./DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = (props) => {
+  if (props.spots === 0) {
+    return "no spots remaining";
+  } else if (props.spots === 1) {
+    return "1 spot remaining"
+  } else {
+    return `${props.spots} spots remaining`;
+  }
 
+}
 export default function DayListItem(props) {
+  
   const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0})
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
