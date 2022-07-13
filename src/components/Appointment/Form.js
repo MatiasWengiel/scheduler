645cd9f3df1008f4c4415fbcf5a044aced17d665
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-13 15:49:50 -0500

    Create Form component and associated stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+
+export default function Form(props) {
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.student || "" );
+  const [interviewer, setInterviewer] = useState(props.interviewer || null )
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={e => e.preventDefault() }>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={interviewers}
+          value={interviewer}
+          onClick={(event) => setInterviewer(event)}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={onSave} >Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -21,6 +21,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,3 +156,5 @@ storiesOf("InterviewerList", module)
     .add("Confirm", () => <Confirm message="Delete the appointment?"  onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
     .add("Status", () => <Status message="Deleting" />)
     .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+    .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+    .add("Form Edit", () => <Form interviewer={1} interviewers={interviewers} student="Matias Wengiel" onSave={action("onSave")} onCancel={action("onCancel")} />)
