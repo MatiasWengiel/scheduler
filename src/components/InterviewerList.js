409d1b6595045823a9238a6e42185a95da1c7486
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-13 11:42:00 -0500

    Refactor to move definition of setInterviewer to InterviewerList, with props.id as an argument, rather than passing props.id to InterviewerListItem and defining the function there

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,14 @@ import "./InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem.js";
 
 export default function InterviewerList(props) {
-  const interviewersArray = props.interviewers.map(prop => <InterviewerListItem key={prop.id} id={prop.id} name={prop.name} avatar={prop.avatar} selected={props.interviewer === prop.id} setInterviewer={props.setInterviewer}/>)
+  const interviewersArray = props.interviewers.map(prop => 
+    <InterviewerListItem 
+      key={prop.id} 
+      name={prop.name} 
+      avatar={prop.avatar} 
+      selected={props.interviewer === prop.id} 
+      setInterviewer={() => props.setInterviewer(prop.id)}
+    />)
 
   return (
     <section className="interviewers">
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,11 +6,11 @@ export default function InterviewerListItem(props) {
   const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
     <img
-    className="interviewers__item-image"
-    src={props.avatar}
-    alt={props.name}
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
      />
     {props.selected && props.name}
   </li>
