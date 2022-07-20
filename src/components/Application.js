0f3c8acf33adb777c142f10bfba7a3596fbbb6c4
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-20 18:10:15 -0500

    Change file to use interview information retrieved from API rather than hard-coded

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,13 +5,14 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
 
-import { getAppointmentsForDay } from '../helpers/selectors'
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors'
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day)
@@ -21,18 +22,21 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8000/api/appointments'),
-      axios.get('http://localhost:8000/api/interviewers')
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
     ]).then ((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
     })
   }, []);
 
-  const appointmentArray = Object.values(dailyAppointments).map(appointment => 
-    <Appointment 
-    key={appointment.id}
-    {...appointment}
-    />)
+  const appointmentArray = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment 
+      key={appointment.id}
+      {...appointment}
+      />)
+    })
 
   return (
     <main className="layout">
