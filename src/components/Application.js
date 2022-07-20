686630eb75bf73cf096ddb72f3a415d5742d0cc4
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-20 16:49:48 -0500

    Update useEffect function to retrieve both days and appointments (and interviewers, for future use) from API using Promise.all. Update dailyAppointments to call getAppointmentsForDay using the new state parameters to render the daily appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,8 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
 
+import { getAppointmentsForDay } from '../helpers/selectors'
+
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -12,16 +14,21 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = []
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
   const setDay = day => setState(prev => ({...prev, day}));
   const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then(res =>  setDays(res.data))
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8000/api/appointments'),
+      axios.get('http://localhost:8000/api/interviewers')
+    ]).then ((all) => {
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
+    })
   }, []);
 
-  const appointmentArray = Object.values(appointments).map(appointment => 
+  const appointmentArray = Object.values(dailyAppointments).map(appointment => 
     <Appointment 
     key={appointment.id}
     {...appointment}
