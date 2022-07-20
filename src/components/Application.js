5b9fc68e1bb566db70c6679f74f99c0fa4257a8f
Matias Wengiel <matiaswengiel@gmail.com>
2022-07-20 16:37:40 -0500

    Refactor Application.js to use a single state object. Create aliased functions that set the day and days parameters within the state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,55 +5,21 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
 
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = []
+  const setDay = day => setState(prev => ({...prev, day}));
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
       .then(res =>  setDays(res.data))
-  }, [])
+  }, []);
 
   const appointmentArray = Object.values(appointments).map(appointment => 
     <Appointment 
@@ -77,8 +43,8 @@ export default function Application(props) {
       />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        day={day}
+        days={state.days}
+        day={state.day}
         onChange={setDay}
         />
       </nav>
