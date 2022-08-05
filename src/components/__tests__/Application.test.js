27d79fa4342c25173d3b97fe7d7ff21c62fbcb5f
Matias Wengiel <matiaswengiel@gmail.com>
2022-08-05 11:20:39 -0500

    Finish test 'loads data, books an interview and reduces the spots remaining for the day by 1'. Add data-testid to DayListItem for test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,10 +6,10 @@ import {
   waitForElement,
   getByText,
   fireEvent,
-  prettyDOM,
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
+  queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -29,14 +29,15 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for the day by 1", async () => {
     const { container } = render(<Application />);
-
+    //Wait for the application to load with data from mock server
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    const appointments = getAllByTestId(container, "appointment");
-    const appointment = appointments[0];
+    //Select the first empty appointment and click on the "Add" button
+    const appointment = getAllByTestId(container, "appointment")[0];
 
     fireEvent.click(getByAltText(appointment, "Add"));
 
+    //Update the name input field with a student's name, select an instructor and save
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" },
     });
@@ -45,6 +46,21 @@ describe("Application", () => {
 
     fireEvent.click(getByText(container, "Save"));
 
-    console.log(prettyDOM(appointment));
+    //Checks to see that we transition() to Saving before rendering the newly booked interview
+    expect(getByText(container, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    //Checks to see that the appointment was created properly, by looking at all of the components in the SHOW element (we already know Lydia is there due to line 52)
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+    expect(getByAltText(appointment, "Edit")).toBeInTheDocument();
+    expect(getByAltText(appointment, "Delete")).toBeInTheDocument();
+
+    //Checks to see that the spots remaining for Monday are updated correctly
+    const day = getAllByTestId(container, "day").find((day) =>
+      getByText(day, "Monday")
+    );
+
+    expect(queryByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
